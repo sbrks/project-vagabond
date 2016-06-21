7e3825d0a039ffefa4af44d10960b5bfd703e999
mattlaguardia <matthewlaguardia@gmail.com>
2016-06-20 18:30:40 -0700

    adds more to posts branch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+  has_many :city_posts
+  has_many :users, through: :city_posts
+
+  def posts_index
+    @posts = Post.all
+    render :index
+  end
+
+  def new_post
+    @post = Post.new
+    render :new
+  end
+
 end
