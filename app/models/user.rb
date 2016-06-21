a54f625404faa9d9c64d3f9a2dc08844803f955b
SBRKS <sbrks3@gmail.com>
2016-06-21 11:35:08 -0700

    edits user show page, adds paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
+
+		#paperclip
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  	
 	BCrypt::Engine.cost = 12
 
-	has_many :posts
+	# has_many :posts
 
 	#validations
 	validates :email, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621020734) do
+ActiveRecord::Schema.define(version: 20160621055443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,13 @@ ActiveRecord::Schema.define(version: 20160621020734) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "username"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "location"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
