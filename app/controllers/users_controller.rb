0c2246fd941851404df0b19f4e9180a2bb3f88c0
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-06-22 14:31:51 -0700

    corrects user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 #only logged in users can see profile pages
  # before_filter :authorize
- 	before_action :logged_in?, only: [:edit, :update :delete]
+ 	before_action :logged_in?, only: [:edit, :update, :delete]
  	before_action :current_user, only: [:show]
 
 	#renders home page
