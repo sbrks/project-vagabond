7e4d1650328bfb8f59bb025f1da052efe5459a11
mattlaguardia <matthewlaguardia@gmail.com>
2016-06-23 14:35:27 -0700

    adds paperclip to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160623192032) do
 
   # These are extensions that must be enabled in order to support this database
@@ -41,9 +40,13 @@ ActiveRecord::Schema.define(version: 20160623192032) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "username"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "location"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_foreign_key "posts", "cities"
