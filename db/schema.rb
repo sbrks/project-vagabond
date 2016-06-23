e5c3f73e4a8046e457bf8ab1138c1512296473d1
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-06-23 12:24:52 -0700

    adds corect user

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623063046) do
+ActiveRecord::Schema.define(version: 20160622024650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "image_url"
-    t.string   "sigil_image"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,8 +28,6 @@ ActiveRecord::Schema.define(version: 20160623063046) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "cities_id"
-    t.integer  "users_id"
     t.integer  "user_id"
   end
 
@@ -40,13 +37,9 @@ ActiveRecord::Schema.define(version: 20160623063046) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "username"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "location"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_foreign_key "posts", "users"
