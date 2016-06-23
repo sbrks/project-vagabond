695339682d550854b7b0011068b7acf1db7052c8
GLINRAEN <MISTRESS.WYN@GMAIL.COM>
2016-06-23 09:32:29 -0700

    add cities sigl migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622024650) do
+ActiveRecord::Schema.define(version: 20160623063046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image_url"
+    t.string   "sigil_image"
   end
 
   create_table "posts", force: :cascade do |t|
